36f8f602f6f06f964dca4e8fa6284fdaf93b0255
Jules <jules.dufraiche@epitech.eu>
2024-10-11 01:14:16 +0200

    feat: Add server.js file for collatask-backend

---
+++ collatask-backend/server.js
@@ -0,0 +1,35 @@
+// server.js
+const express = require('express');
+const bodyParser = require('body-parser');
+const authRoutes = require('./routes/auth');
+const projectRoutes = require('./routes/project');
+const projectAssignmentRoutes = require('./routes/projectAssignments');
+const userBoardsRoutes = require('./routes/userProjects');
+
+const app = express();
+
+// Middleware
+app.use(bodyParser.json());
+
+// Routes
+app.use('/api/auth', authRoutes);
+app.use('/api/projects', projectRoutes);
+app.use('/api/project-assignments', projectAssignmentRoutes);
+app.use('/api/user-projects', userBoardsRoutes);
+
+// Error handler
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
+// Export the app
+module.exports = app;
+
+// Start the server only if this file is executed directly
+if (require.main === module) {
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+}
