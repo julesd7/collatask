9efe52074fe4ba94b5126c4e2e33a9b33cbfb392
Jules <jules.dufraiche@epitech.eu>
2024-10-25 19:16:41 +0200

    feat: Add authentication middleware
    Add a new file, authMiddleware.js, to handle authentication using JSON Web Tokens (JWT). The middleware function authenticateJWT is responsible for verifying the token provided in the request headers and decoding it. If the token is valid, the user information is added to the request object. If the token is missing or invalid, the middleware sends an appropriate response status code.

---
+++ collatask-backend/middleware/authMiddleware.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken');
+const { pool } = require('../db');
+
+const authenticateJWT = async (req, res, next) => {
+    const token = req.headers.authorization?.split(' ')[1];
+
+    if (!token) {
+        return res.sendStatus(401);
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded;
+
+        const user = await pool.query('SELECT * FROM users WHERE id = $1', [decoded.id]);
+        req.user = { ...req.user, ...user.rows[0] };
+
+        next();
+    } catch (err) {
+        return res.sendStatus(403);
+    }
+};
+
+module.exports = { authenticateJWT };
