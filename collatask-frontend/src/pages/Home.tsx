d005c4d6222e8afe2cfdac5946bb6f8b4ea45158
Jules <jules.dufraiche@epitech.eu>
2024-11-07 21:42:07 +0100

    feat(front): Add Home component with logout link

---
+++ collatask-frontend/src/pages/Home.tsx
@@ -0,0 +1,15 @@
+//Home.tsx
+
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+      <Link to="/logout">Logout</Link>
+    </div>
+  )
+}
+
+export default Home
