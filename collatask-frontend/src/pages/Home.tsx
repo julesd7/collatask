dc862b9fa2cd84e3e7cb47cabea4cdc510720d25
Jules <jules.dufraiche@epitech.eu>
2024-11-23 02:08:12 +0100

    refactor(front): Update logout functionality in Home page

--- collatask-frontend/src/pages/Home.tsx
+++ collatask-frontend/src/pages/Home.tsx
@@ -1,13 +1,25 @@
 // Home.tsx
 
 import React from 'react'
-import { Link } from 'react-router-dom'
+import axios from 'axios';
 
 const Home: React.FC = () => {
+
+  const handleSignOut = async () => {
+    try {
+      await axios.post(`${import.meta.env.VITE_APP_URL}/api/auth/logout`, {}, {
+        withCredentials: true,
+      });
+      window.location.reload();
+    } catch (error) {
+      console.error('An error occurred while signing out.');
+    }
+  }
+
   return (
     <div>
       <h1>Home</h1>
-      <Link to="/logout">Logout</Link>
+      <p onClick={() => handleSignOut()} style={{ cursor: 'pointer' }}>Logout</p>
     </div>
   )
 }
