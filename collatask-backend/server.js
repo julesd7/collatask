8f7e53580f7a17f23126526b8f0afc3dd995a497
Jules <jules.dufraiche@epitech.eu>
2025-02-21 23:43:10 +0100

    feat: Add messages table and integrate Socket.io for real-time communication

--- collatask-backend/server.js
+++ collatask-backend/server.js
@@ -1,7 +1,11 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const http = require("http");
+const { Server } = require("socket.io");
+const pg = require("pg");
+require("dotenv").config();
 
 // Import the routes
 const authRoutes = require('./routes/auth');
@@ -15,30 +19,42 @@ const boardRoutes = require('./routes/boards');
 const contactRoutes = require('./routes/contact');
 const helloRoute = require('./routes/hello');
 
-const app = express();
+const socketHandler = require('./socketHandler');
 
-app.use(cors({
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    credentials: true,
+  },
+});
+
+// Middlewares
+app.use(
+  cors({
     origin: process.env.FRONTEND_URL,
     credentials: true,
-  }));
-
-// Middleware
+  })
+);
 app.use(bodyParser.json());
 app.use(cookieParser());
 
 // Routes
-app.use('/api/auth', authRoutes);
-app.use('/api/hello', helloRoute);
-app.use('/api/user', accountRoutes);
-app.use('/api/projects', projectRoutes);
-app.use('/api/project-assignments', projectAssignmentRoutes);
-app.use('/api/user-projects', userProjectsRoutes);
-app.use('/api/cards', cardRoutes);
-app.use('/api/card-assignments', cardAssignmentRoutes);
-app.use('/api/boards', boardRoutes);
-app.use('/api/contact', contactRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/hello", helloRoute);
+app.use("/api/user", accountRoutes);
+app.use("/api/projects", projectRoutes);
+app.use("/api/project-assignments", projectAssignmentRoutes);
+app.use("/api/user-projects", userProjectsRoutes);
+app.use("/api/cards", cardRoutes);
+app.use("/api/card-assignments", cardAssignmentRoutes);
+app.use("/api/boards", boardRoutes);
+app.use("/api/contact", contactRoutes);
 
-// Error handler
+// Socket.io
+socketHandler(io);
+
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Something broke!');
@@ -47,10 +63,9 @@ app.use((err, req, res, next) => {
 // Export the app
 module.exports = app;
 
-// Start the server only if this file is executed directly
 if (require.main === module) {
     const PORT = process.env.PORT || 3000;
-    app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`);
+    server.listen(PORT, () => {
+        console.log(`WebSocket is running on port ${PORT}`);
     });
 }
